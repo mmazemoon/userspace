701abbc6e34d59f7ee15e1c185b83bef0a372f89
Mark Azemoon <mmazemoon@gmail.com>
2016-02-24 15:26:57 -0800

    Setup eight API endpoints for users resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-
+  #resources :users
+  get  'users' => 'users#index'
+  post 'users' => "users#create"
+  get 'users/new' => "users#new"
+  get 'users/:id/edit' => "users#edit"
+  get "users/:id" => "users#show"
+  put "users/:id" => "users#update"
+  patch "users/:id" => "users#update"
+  delete "users/:id" => "users#destroy"
 end
