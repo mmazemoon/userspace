e79566b0ae2f4aad2c712113da07cac25dfaeeea
Mark Azemoon <mmazemoon@gmail.com>
2016-03-09 14:06:59 -0800

    Add all 7 Users API controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def index
     render text: "Im in the index action!"
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
