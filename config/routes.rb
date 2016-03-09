a42b744639ab8ffd98196f89c2d3680191a932e8
Mark Azemoon <mmazemoon@gmail.com>
2016-03-09 14:06:18 -0800

    Re-arrange User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # resources :users
   get  'users' => 'users#index', :as => "users"
-  post 'users' => "users#create"
+  get "users/:id" => "users#show", :as => "user"
   get 'users/new' => "users#new", :as => "new_user"
   get 'users/:id/edit' => "users#edit", :as => "edit_user"
-  get "users/:id" => "users#show", :as => "user"
-  patch "users/:id" => "users#update"
+  post 'users' => "users#create"
   put "users/:id" => "users#update"
+  patch "users/:id" => "users#update"
   delete "users/:id" => "users#destroy"
 end
